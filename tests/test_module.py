2b3d45991aadc34cc139eca2a5a05347ff90f110
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-07 11:39:48 -0500

    Added sample tests

---
+++ tests/test_module.py
@@ -0,0 +1,22 @@
+"""This module contains a variety of tests for the module.py module"""
+import unittest
+
+from joe_template import MyClass
+
+ 
+class TestStuff(unittest.TestCase):
+    """Tests for MyClass class"""
+    def setUp(self):
+        pass
+
+    def test_myclass(self):
+        """Test if Filter object is created properly"""
+        # Comments are nice too
+        words = 'Here are the words!'
+        mc = MyClass(words)
+
+        # Here's another
+        self.assertTrue(mc.words == 'Here are the words!')
+
+if __name__ == '__main__':
+    unittest.main()
