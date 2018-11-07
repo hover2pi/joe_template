2d2d83c30425e325ce8c5d2208a0824fb463eacd
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-07 11:13:54 -0500

    Create setup_conda_env.sh

---
+++ ci/setup_conda_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Creating a Python $PYTHON_VERSION environment"
+conda create -n my_env python=$PYTHON_VERSION || exit 1
+source activate my_env
+
+echo "Installing packages..."
+conda install flake8 beautifulsoup4 lxml numpy astropy
