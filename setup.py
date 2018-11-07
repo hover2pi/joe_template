c8a052791530cc4bcd3a1f380330e06fe6fa11b6
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-07 11:26:40 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='joe_template',
+      version=0.1,
+      description='Nice short description',
+      install_requires=['astropy', 'numpy'],
+      author='Joe Filippazzo',
+      author_email='jfilippazzo@stsci.edu',
+      license='MIT',
+      url='https://github.com/hover2pi/joe_template/',
+      long_description='Nice looooooooooong description',
+      zip_safe=False,
+      use_2to3=False
+)
