6696baae0ba6b3d36660fcd0b3212824575b2577
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-07 13:38:28 -0500

    Added coveralls

--- joe_template/__init__.py
+++ joe_template/__init__.py
@@ -1,6 +1,2 @@
 """The directory of modules should have an __init__.py with a docstring too"""
-<<<<<<< HEAD
 from .joe_module import MyClass
-=======
-from . import module
->>>>>>> 27ed4278391a6c89d45c6edad7db3a43e6f6cee3
